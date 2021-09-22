a5adc4ab6b728ab4d870ccafa4257ce6d3aa552b
김민수 <88013690+clobot-hans@users.noreply.github.com>
2021-09-22 15:50:50 +0900

    Custom progress bar 적용

--- react-scripts/src/components/molecules/CustomizedProgressBars/CustomizedProgressBars.js
+++ react-scripts/src/components/molecules/CustomizedProgressBars/CustomizedProgressBars.js
@@ -7,16 +7,26 @@ require('moment-timezone');
 
 Moment.tz.setDefault("Asia/Seoul");
 
-export default function CustomizedProgressBars() {
+export default function CustomizedProgressBars(props) {
     const [afterTokenSale, setAfterTokenSale] = React.useState(false);
+    const [afterTokenSaleOpenContext, setAfterTokenSaleOpenContext] = React.useState("");
+    const [afterTokenSaleCloseContext, setAfterTokenSaleCloseContext] = React.useState("");
 
     const checkIsAfterTokenSale = () => {
         const today = Moment();
-        const isAfterTokenSale = today.isAfter(Moment('26-06-2021 00:00:00', 'DD-MM-YYYY hh:mm:ss'));
+        const isAfterTokenSale = today.isAfter(Moment(props.saleDate, 'DD-MM-YYYY hh:mm:ss'));
 
         console.log("isAfterTokenSale: " + isAfterTokenSale);
 
         setAfterTokenSale(isAfterTokenSale);
+
+        if (props.isStart) {
+            setAfterTokenSaleOpenContext("Public sale starts in ");
+            setAfterTokenSaleCloseContext("");
+        } else {
+            setAfterTokenSaleOpenContext("Public sale closes in ");
+            setAfterTokenSaleCloseContext("");
+        }
     };
 
     React.useEffect(() => {
@@ -32,12 +42,9 @@ export default function CustomizedProgressBars() {
                 align={"left"}
             >
                 <strong>
-                    { afterTokenSale ? 'Public sale closes in ' : 'Public sale starts in ' }
-                    { afterTokenSale ? <Countdown
-                        date={Date.now() + Moment('10-07-2021 00:00:00', 'DD-MM-YYYY hh:mm:ss').diff(Moment(), 'milliseconds')}
-                        renderer={props => <span>{props.days} d {props.hours} h {props.minutes} m {props.seconds} s</span>}
-                    /> : <Countdown
-                        date={Date.now() + Moment('26-06-2021 00:00:00', 'DD-MM-YYYY hh:mm:ss').diff(Moment(), 'milliseconds')}
+                    { afterTokenSale ? afterTokenSaleCloseContext : afterTokenSaleOpenContext }
+                    { afterTokenSale ? "" : <Countdown
+                        date={Date.now() + Moment(props.saleDate, 'DD-MM-YYYY hh:mm:ss').diff(Moment(), 'milliseconds')}
                         renderer={props => <span>{props.days} d {props.hours} h {props.minutes} m {props.seconds} s</span>}
                     />}
                 </strong>
